46d7b9a5f6c223090777b210318bb821ea7f70f8
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-09 10:18:32 +0300

    add employees table

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+import { Header } from "../components";
+import { employeesData, employeesGrid } from "../constants";
 
 const Employees = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Employees" category="Page" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        width={"auto"}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Employees
+export default Employees;
