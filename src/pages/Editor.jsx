3d7f973f7467db53d957e64cb41850123bb1cdee
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-09 11:42:38 +0300

    Update Editor.jsx

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import { Header } from "../components";
+import { EditorData } from "../constants";
 
 const Editor = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Editor" category="Page" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
+      </RichTextEditorComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
