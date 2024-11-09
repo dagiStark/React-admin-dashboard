e06dd8da1fb9429faadd7f58efb3344f940b0923
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-09 20:31:11 +0300

    add dark theme to the main page

--- src/App.js
+++ src/App.js
@@ -27,11 +27,16 @@ import {
 } from "./pages";
 
 const App = () => {
-  const { activeMenu, themeSettings, setThemeSettings, currentColor } =
-    useStateContext();
+  const {
+    activeMenu,
+    themeSettings,
+    setThemeSettings,
+    currentColor,
+    currentMode,
+  } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
@@ -62,7 +67,7 @@ const App = () => {
           )}
 
           <div
-            className={` dark:bg-main-bg bg-main-bg min-h-screen w-full
+            className={` dark:bg-main-dark-bg bg-main-bg min-h-screen w-full
               ${activeMenu ? " md:ml-72 " : "  flex-2"}`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
